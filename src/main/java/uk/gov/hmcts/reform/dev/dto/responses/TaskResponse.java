7b2ae48c03e65a93126669e3137c28dc1267031d
tests <neeraj.rai8@proton.me>
2026-04-05 23:51:02 +0100

    add mapstruct for tasks

---
+++ src/main/java/uk/gov/hmcts/reform/dev/dto/responses/TaskResponse.java
@@ -0,0 +1,34 @@
+package uk.gov.hmcts.reform.dev.dto.responses;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import uk.gov.hmcts.reform.dev.models.Task.TaskStatus;
+
+/**
+ * DTO for task responses. Added HATEOAS links for client simplicity
+ */
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TaskResponse {
+
+    private Long id;
+    private String title;
+    private String description;
+    private TaskStatus status;
+    private LocalDateTime dueDate;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private Short version;
+
+    /**
+     * HATEOAS links. E.g.
+     * {"self": "v1/tasks/123", "update": "v1/tasks/123", "delete": "v1/tasks/123"}
+     */
+    private Map<String, String> links;
+}
