118106bbf3cc9d6a5e6e24010335448af53797f9
tests <neeraj.rai8@proton.me>
2026-04-06 02:25:07 +0100

    add task controller with hateoas

--- src/main/java/uk/gov/hmcts/reform/dev/controllers/TaskController.java
+++ src/main/java/uk/gov/hmcts/reform/dev/controllers/TaskController.java
@@ -1,16 +1,76 @@
 package uk.gov.hmcts.reform.dev.controllers;
 
-import static org.springframework.http.ResponseEntity.ok;
-
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import uk.gov.hmcts.reform.dev.dto.requests.CreateTaskRequest;
+import uk.gov.hmcts.reform.dev.dto.requests.UpdateTaskRequest;
+import uk.gov.hmcts.reform.dev.dto.responses.TaskResponse;
+import uk.gov.hmcts.reform.dev.services.TaskService;
+import uk.gov.hmcts.reform.dev.utils.HateoasLinkBuilder;
+
+import java.net.URI;
+import java.util.List;
 
 @RestController
+@RequestMapping("/v1/tasks")
 public class TaskController {
 
-    @GetMapping(value = "/tasks", produces = "application/json")
-    public ResponseEntity<String> getExampleCase() {
-        return ok("hello");
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @PostMapping
+    public ResponseEntity<TaskResponse> createTask(
+        @Valid @RequestBody CreateTaskRequest request) {
+
+        TaskResponse createdTask = taskService.createTask(request);
+        TaskResponse taskResponse = HateoasLinkBuilder.addHateoasLinks(createdTask);
+
+        URI uri = ServletUriComponentsBuilder
+            .fromCurrentRequest()
+            .path("/{id}")
+            .buildAndExpand(createdTask.getId())
+            .toUri();
+
+        return ResponseEntity.created(uri).body(taskResponse);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TaskResponse>> getTasks() {
+        List<TaskResponse> tasks = taskService.getTasks();
+        List<TaskResponse> taskResponses = tasks.stream()
+            .map(HateoasLinkBuilder::addHateoasLinks)
+            .toList();
+
+        return ResponseEntity.ok(taskResponses);
+
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<TaskResponse> updateTaskStatus(
+        @PathVariable Long id,
+        @Valid @RequestBody UpdateTaskRequest request) {
+
+        TaskResponse updateTaskStatus = taskService.updateTaskStatus(id, request);
+        TaskResponse taskResponse = HateoasLinkBuilder.addHateoasLinks(updateTaskStatus);
+
+        return ResponseEntity.ok(taskResponse);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+        return ResponseEntity.noContent().build();
     }
 }
---
+++ src/main/java/uk/gov/hmcts/reform/dev/utils/HateoasLinkBuilder.java
@@ -0,0 +1,51 @@
+package uk.gov.hmcts.reform.dev.utils;
+
+import uk.gov.hmcts.reform.dev.dto.responses.TaskResponse;
+import uk.gov.hmcts.reform.dev.models.Task;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Utility class for building HATEOAS links for tasks.
+ * Generating links to decouple client url generation.
+ */
+public class HateoasLinkBuilder {
+
+    private static final String BASE_PATH = "v1/tasks";
+
+    private HateoasLinkBuilder() {
+
+    }
+
+    private static Map<String, String> buildLinks(Long taskId, Task.TaskStatus taskStatus) {
+        Map<String, String> links = new HashMap<>();
+
+        String selfLink = BASE_PATH + "/" + taskId;
+        links.put("self", selfLink);
+
+        if (taskStatus != Task.TaskStatus.DISPOSED) {
+            links.put("update", selfLink);
+        }
+
+        links.put("delete", selfLink);
+
+        return links;
+    }
+
+    public static TaskResponse addHateoasLinks(TaskResponse taskResponse) {
+        Map<String, String> links = HateoasLinkBuilder.buildLinks(taskResponse.getId(), taskResponse.getStatus());
+
+        return TaskResponse.builder()
+            .id(taskResponse.getId())
+            .title(taskResponse.getTitle())
+            .description(taskResponse.getDescription())
+            .status(taskResponse.getStatus())
+            .dueDate(taskResponse.getDueDate())
+            .version(taskResponse.getVersion())
+            .createdAt(taskResponse.getCreatedAt())
+            .updatedAt(taskResponse.getUpdatedAt())
+            .links(links)
+            .build();
+    }
+}
